39e0c4361aafbd172fb37d2e2d8cf80077b19147
Tony <piunov.doc@gmail.com>
2021-12-11 20:17:37 +0300

    Deleted unused file

--- api test.py
+++
@@ -1,8 +0,0 @@
-import requests, json
-
-headers = {'Content-Type': 'application/json'}
-
-
-r = requests.get(url='https://api.telegram.org/bot5091246162:AAGmcQUi_lhpkvmE5GeiDC2bFUkuNb489-Q/getUpdates', headers=headers)
-#r = requests.post(url='https://api.telegram.org/bot5091246162:AAGmcQUi_lhpkvmE5GeiDC2bFUkuNb489-Q/sendMessage?chat_id=323754099&text=иди%0нахуй', headers=headers)
-print(r.json())
